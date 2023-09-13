9df7a2febafa3a0a89467d92c8b0f256e0328bd2
Dan Filipski <dfilipski@firemail.cc>
2023-09-13 14:21:48 -0400

    Add project files.

---
+++ lab1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	double interest_rate;
+	std::cout << "Please enter the annual interest rate as a percentage (e.g., 15 for 15%) (enter 0 to quit): ";
+
+
+	std::cin >> interest_rate;
+}
